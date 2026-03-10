831487dcd3e8dbd6d8da7411207202961c484810
Danny Geurts <dannygeurts@gmail.com>
2026-03-10 12:03:10 +0100

    auto-commit

--- sql/seed_content_categories.sql
+++ sql/seed_content_categories.sql
@@ -10,52 +10,10 @@ with category_taxonomy as (
 seed_rows(name, slug, description, sort_order) as (
   values
     (
-      'Gratis Content',
-      'gratis',
+      'Symbolen',
+      'symbolen',
       'Toegang tot gratis oefeningen, meditaties en wellness tools. Begin vandaag nog met jouw welzijnsreis.',
-      80
-    ),
-    (
-      'Cognitie & Inzicht',
-      'cognitie-inzicht',
-      'Verken je gedachten en mentale processen. Krijg inzicht in hoe je denkt en beslissingen maakt.',
-      10
-    ),
-    (
-      'Emoties & Innerlijke Beleving',
-      'emoties-innerlijke-beleving',
-      'Verbind met je emoties en innerlijke wereld. Ontwikkel emotioneel bewustzijn en veerkracht.',
-      20
-    ),
-    (
-      'Gedrag & Interactie',
-      'gedrag-interactie',
-      'Verbeter je sociale vaardigheden en interacties. Leer patronen herkennen in jouw gedrag.',
-      30
-    ),
-    (
-      'Lichaam & Zintuigen',
-      'lichaam-zintuigen',
-      'Ontwikkel lichaamsbesef en zintuiglijk bewustzijn. Voel wat je lichaam je vertelt.',
-      40
-    ),
-    (
-      'Natuur & Symbolische Kracht',
-      'natuur-symbolische-kracht',
-      'Ontdek de helende kracht van natuur en symboliek. Vind verbinding met jezelf en je omgeving.',
-      50
-    ),
-    (
-      'Zingeving, Ritualen & Spiritualiteit',
-      'zingeving-ritualen-spiritualiteit',
-      'Vind betekenis door rituelen en spirituele praktijken. Verdiep je innerlijke rust en richting.',
-      60
-    ),
-    (
-      'Specifieke Doelgroepen & Context',
-      'specifieke-doelgroepen-context',
-      'Gerichte ondersteuning voor jouw unieke situatie en context. Voor iedereen een passende route.',
-      70
+      90
     )
 )
 insert into public.content_terms (
